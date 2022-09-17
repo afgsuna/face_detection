7028b9d0e2aa5dddffc0d4385bfa3a1184ace651
afgsuna <afgsuna@yahoo.com>
2022-09-17 22:12:54 +0200

    original push

--- face_detection.py
+++ face_detection.py
@@ -6,29 +6,26 @@ cascPath = os.path.dirname(cv2.__file__)+"/data/haarcascade_frontalface_default.
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 
-myvid_cap = cv2.myvid_cap(0)
+video_capture = cv2.VideoCapture(0)
 
 while True:
-    # Capture all frame second by second
-    ret, frames = myvid_cap.read()
-
+    # Capture frame-by-frame
+    ret, frames = video_capture.read()
     gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
-
     faces = faceCascade.detectMultiScale(
-        gray, scaleFactor = 1.1, minNeighbors = 5, minSize = (20, 20), flags=cv2.CASCADE_SCALE_IMAGE
+        gray,
+        scaleFactor = 1.1,
+        minNeighbors = 5,
+        minSize = (30, 30),
+        flags = cv2.CASCADE_SCALE_IMAGE
     )
-
+    
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frames, (x, y), (x+w, y+h), (0, 230, 0), 2)
-
+        cv2.rectangle(frames, (x, y), (x+w, y+h), (0, 255, 0), 2)
     # Display the resulting frame
     cv2.imshow('Video', frames)
-
+    
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
-    
-
-myvid_cap.release()
-cv2.closeallwin()
